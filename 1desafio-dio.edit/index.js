11e392a29af72d53872ba31cd23234cb5b43cb76
ArielABdev <arielalmeida4545@gmail.com>
2023-12-30 01:00:39 -0300

    1desafio

---
+++ 1desafio-dio.edit/index.js
@@ -0,0 +1,27 @@
+// Defina o nome e a quantidade de experiência (XP) do herói
+let nomeDoHeroi = "Nome do Herói";
+let experienciaDoHeroi = 7500; // Altere o valor da XP conforme necessário
+
+// Utilize uma estrutura de decisão para determinar o nível do herói com base na XP
+let nivelDoHeroi;
+
+if (experienciaDoHeroi < 1000) {
+    nivelDoHeroi = "Ferro";
+} else if (experienciaDoHeroi >= 1001 && experienciaDoHeroi <= 2000) {
+    nivelDoHeroi = "Bronze";
+} else if (experienciaDoHeroi >= 2001 && experienciaDoHeroi <= 5000) {
+    nivelDoHeroi = "Prata";
+} else if (experienciaDoHeroi >= 5001 && experienciaDoHeroi <= 7000) {
+    nivelDoHeroi = "Ouro";
+} else if (experienciaDoHeroi >= 7001 && experienciaDoHeroi <= 8000) {
+    nivelDoHeroi = "Platina";
+} else if (experienciaDoHeroi >= 8001 && experienciaDoHeroi <= 9000) {
+    nivelDoHeroi = "Ascendente";
+} else if (experienciaDoHeroi >= 9001 && experienciaDoHeroi <= 10000) {
+    nivelDoHeroi = "Imortal";
+} else {
+    nivelDoHeroi = "Radiante";
+}
+
+// Exibir mensagem com o nome do herói e seu nível
+console.log(`O Herói de nome ${nomeDoHeroi} está no nível de ${nivelDoHeroi}`);
